bcb51767d2298c086e1909662328650013019fde
emersonreyes727 <emersonreyes727@gmail.com>
2017-02-01 10:45:48 -0800

    Loop platform

--- Haunted Zombie Rush/Assets/Scripts/Platform.cs
+++ Haunted Zombie Rush/Assets/Scripts/Platform.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Platform : MonoBehaviour {
 
 	[SerializeField] private float platformSpeed;
+	[SerializeField] private float resetPlatformPosition = -34.0f;
+	[SerializeField] private float startPlatformPosition = 28.80f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +17,9 @@ public class Platform : MonoBehaviour {
 	void Update () {
 		transform.Translate (Vector3.left * (platformSpeed * Time.deltaTime));
 
-
+		if (transform.localPosition.x <= resetPlatformPosition) {
+			Vector3 newPosition = new Vector3 (startPlatformPosition, transform.position.y, transform.position.z);
+			transform.position = newPosition;
+		}
 	}
 }
