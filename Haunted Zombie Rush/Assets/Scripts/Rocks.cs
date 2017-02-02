cbdbc5778f799da3c13925f1155f3680270f6086
emersonreyes727 <emersonreyes727@gmail.com>
2017-02-01 16:15:35 -0800

    Add script to move rocks up/down

--- Haunted Zombie Rush/Assets/Scripts/Rocks.cs
+++ Haunted Zombie Rush/Assets/Scripts/Rocks.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocks : Platform {
+	[SerializeField] Vector3 topPosition;
+	[SerializeField] Vector3 bottomPosition;
+	[SerializeField] private float rockSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
+		StartCoroutine (Move (bottomPosition));
 	}
 	
 	// Update is called once per frame
 	public override void Update () {
 		base.Update ();
+	}
+
+	IEnumerator Move (Vector3 target) {
+		while (Mathf.Abs((target - transform.localPosition).y) > 0.20f) {
+			Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+			transform.localPosition += direction * (rockSpeed * Time.deltaTime);
+
+			yield return null;
+		}
+
+		print ("target reached");
+
+		yield return new WaitForSeconds (0.5f);
+
+		Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+		StartCoroutine (Move (newTarget));
 	}
 }
