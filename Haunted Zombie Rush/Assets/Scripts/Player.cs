90a661224ff987968f20b4e4b824fd053882f916
emersonreyes727 <emersonreyes727@gmail.com>
2017-02-06 09:40:56 -0800

    Add collision to rocks

--- Haunted Zombie Rush/Assets/Scripts/Player.cs
+++ Haunted Zombie Rush/Assets/Scripts/Player.cs
@@ -1,39 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour {
 
-	[SerializeField] private float jumpForce = 100f;
-	[SerializeField] private AudioClip sfxJump;
+	[SerializeField] private float jumpForce = 100f; //2
+	// instead of dragging audio clip to audio source directly
+	// you create a separate audio clip to easily play different sounds.
+	[SerializeField] private AudioClip sfxJump; //3
+	[SerializeField] private AudioClip sfxDeath;
 
-	private Animator anim;
-	private Rigidbody rigidBody;
-	private bool jump = false;
-	private AudioSource audioSource;
+	private Animator anim; //1
+	private Rigidbody rigidBody; //2
+	private AudioSource audioSource; //3
+
+	private bool jump = false; //2
+
+	void Awake () {
+		Assert.IsNotNull (sfxJump);
+		Assert.IsNotNull (sfxDeath);
+	}
 
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator> ();	
-		rigidBody = GetComponent<Rigidbody> ();
-		audioSource = GetComponent<AudioSource> ();
+		anim = GetComponent<Animator> (); //1
+		rigidBody = GetComponent<Rigidbody> (); //2
+		audioSource = GetComponent<AudioSource> (); //3
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
-			anim.Play ("Jump");
-			audioSource.PlayOneShot (sfxJump); // playoneshot means play once
-			rigidBody.useGravity = true;
-			jump = true;
+			anim.Play ("Jump"); //1
+			audioSource.PlayOneShot (sfxJump); //3, playoneshot means play once
+			rigidBody.useGravity = true; //2
+			jump = true; //1
 		}
 	}
 
-	void FixedUpdate () {
-		if (jump == true) {
-			jump = false;
-			rigidBody.velocity = new Vector2 (0, 0);
-			rigidBody.AddForce (new Vector2 (0, jumpForce), ForceMode.Impulse);
+	void FixedUpdate () { //2
+		if (jump == true) { //2
+			jump = false;//2
+			rigidBody.velocity = new Vector2 (0, 0); //2
+			rigidBody.AddForce (new Vector2 (0, jumpForce), ForceMode.Impulse);//2
+		}
+	}
+
+	//4, all 4 - build in collision detector
+	void OnCollisionEnter (Collision collision) { 
+		if (collision.gameObject.tag == "obstacle") {
+			rigidBody.AddForce (new Vector2 (-50, 20), ForceMode.Impulse);
+			rigidBody.detectCollisions = false;
+			audioSource.PlayOneShot (sfxDeath);
 		}
 	}
 }
